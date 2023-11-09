d54952dd8a3cdccae54dc6dd34fcf92281099301
CrazyJoey4 <crazyjoeyooi@gmail.com>
2023-11-09 13:31:34 +0800

    Update attendancePage.php

--- ForFYP/attendancePage.php
+++ ForFYP/attendancePage.php
@@ -5,15 +5,7 @@
     <title>Attendance</title>
     <link rel="icon" href="media/hr-icon.png">
     <?php include "./navBar.php"; ?>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
-    <script src="jsfiles/faceapi/face-api.min.js"></script>
-
+    
     <script defer src="jsfiles/faceapi/face-api.min.js"></script>
     <script type="module" defer src="jsfiles/attendance.js"></script>
 
@@ -22,7 +14,6 @@
 <body>
     <div class="wrap">
         <h1>Attendance</h1>
-
 
         <div class="toClockWrap">
             <div class="videoCapture">
